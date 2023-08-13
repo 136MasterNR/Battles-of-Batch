3c9c914f42007abe614cc4c40fd77eb41b8f5ef8
136MasterNR <62593379+136MasterNR@users.noreply.github.com>
2023-08-14 01:04:49 +0300

    2nd Anniversary!!
    I wasn't able to finish the combat update in time because I was very busy irl. Additionally, I have been brainstorming the entire time, trying to find the best way to implement the combat update.
    
    + fixed bug where enemies' final total attack wouldn't reset and would decrease the player's HP by all the previous attacks +the current one.

--- data/scripts/game/acts/ENEMY_ATK.cmd
+++ data/scripts/game/acts/ENEMY_ATK.cmd
@@ -5,9 +5,9 @@ EXIT /B 0
 :INDIVIDUAL <Enemy>
 :: Reset variables
 FOR /F "TOKENS=1DELIMS==" %%A IN ('SET ENEMY.ATTACK.AMOUNT.') DO (
-	SET /A %%A=0
+	SET %%A=
 )
-::Get the enemy
+:: Get the enemy
 FOR /F "TOKENS=2DELIMS=." %%1 IN ("%1") DO (
 	SET TMP.EN=%%1
 )
@@ -26,25 +26,26 @@ IF !ENEMY.HP.NOW.%TMP.EN%! LEQ 0 (
 :: Update turns
 CALL "%SCRIPTS_GAME%\turn.cmd"
 
-::Calculate the final total damage by extracting the ATK values from the enemy
+:: Extract the ATK values from the enemy
 SETLOCAL ENABLEDELAYEDEXPANSION
 FOR /F "TOKENS=1,2DELIMS=," %%A IN ("!ENEMY.ATK.AMOUNT.%TMP.EN%!") DO (
 	ENDLOCAL
 	SET /A "ENEMY.ATTACK.AMOUNT.%TMP.EN%=%random% %% %%A %%B"
 )
 
+:: Calculate final total damage
+SET ENEMY.ATTACK.AMOUNT=0
 FOR /F "TOKENS=2DELIMS==" %%A IN ('SET ENEMY.ATTACK.AMOUNT.') DO (
 	SET /A ENEMY.ATTACK.AMOUNT+=%%A
 )
 
-::Decrease player's HP accordingly
+:: Decrease player's HP accordingly
 SET /A "PLAYER.HP.NOW=PLAYER.HP.NOW -ENEMY.ATTACK.AMOUNT"
 IF %PLAYER.HP.NOW% LSS 0 (SET "PLAYER.HP.NOW=0")
-
 EXIT /B 0
 
 
-:SIMULTANEOUS
+:SIMULTANEOUS [Not Used]
 :: Reset variables
 FOR /F "TOKENS=1DELIMS==" %%A IN ('SET ENEMY.ATTACK.AMOUNT.') DO (
 	SET /A %%A=0
