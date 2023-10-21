6dd9ab5a481aad3843daeec2d3e22c1abe34da87
136 MasterGamingGR <nr136master@gmail.com>
2023-10-21 18:24:26 +0300

    Extended Debug Features

--- data/cmd/debug.cmd
+++ data/cmd/debug.cmd
@@ -1,7 +1,21 @@
 @ECHO OFF
+
+SET ARG=%1
+IF DEFINED ARG (
+	CLS
+	GOTO %ARG%
+)
+
 IF NOT DEFINED DM (
 	ECHO.There was an error.
 	EXIT /B 0
 )
+
 IF EXIST "%DM%\LET.DEBUG" (DEL "%DM%\LET.DEBUG"&&ECHO.Debug mode disabled.&SET "DEBUG.MODE=") ELSE ECHO.>"%DM%\LET.DEBUG"&&ECHO.Debug mode enabled.&SET "DEBUG.MODE=TRUE"
-@ECHO ON
+ECHO ON
+@EXIT /B 0
+
+:wscript
+ECHO.[H
+TASKLIST /FO LIST | FINDSTR /C:"wscript"
+GOTO wscript
