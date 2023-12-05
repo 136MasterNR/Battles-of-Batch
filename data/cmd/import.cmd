00a85d939150363b4672a94f54b79fa13b7939ec
136 MasterGamingGR <nr136master@gmail.com>
2023-12-05 23:42:10 +0200

    Update import.cmd

--- data/cmd/import.cmd
+++ data/cmd/import.cmd
@@ -1,5 +1,7 @@
 @ECHO OFF
 ::CHCP 437>NUL
-IF EXIST "%*.ans" (TYPE %*.ans)ELSE ECHO.ANSI with name "%*.ans" was not found.
+IF EXIST "%*.ans" (
+	FOR /F "DELIMS=" %%A IN (%*.ans) DO ECHO.%%A
+) ELSE ECHO.ANSI with name "%*.ans" was not found.
 CHCP 65001>NUL
 @ECHO ON
