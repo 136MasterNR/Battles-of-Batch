3afea24d1a2a81e7b8003eb91e97df81ee684949
136 MasterGamingGR <nr136master@gmail.com>
2023-12-29 14:02:21 +0200

    Release of Bob's Renderer

---
+++ data/cmd/coolrenderer.cmd
@@ -0,0 +1,29 @@
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:RENDER-LOOP
+IF NOT EXIST ".\buffer_layer_1.cmd" EXIT
+
+:: Memory optimization
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET LAYERS=1
+:RENDER-BUFFER
+:: Call the buffer for all found layers
+CALL ".\buffer_layer_!LAYERS!.cmd"
+SET /A LAYERS+=1
+IF EXIST ".\buffer_layer_!LAYERS!.cmd" GOTO RENDER-BUFFER
+
+:RENDER-DISPLAY
+:: Display the lines ...
+FOR /L %%y IN (1, 1, %LINES%) DO (
+	:: ... display all additional layers linked to this line
+	FOR /L %%l in (1, 1, !LAYERS!) DO (
+		SET /p "=[%%yH!layermap[%%l,%%y]!" <NUL
+	)
+)
+:: Clear buffer memory
+ENDLOCAL
+
+:: End of rendering, repeat
+GOTO RENDER-LOOP
---
+++ data/cmd/testrender.bat
@@ -0,0 +1,132 @@
+@ECHO OFF
+ECHO.[?25l
+
+(
+	ECHO SET "layermap[1,1]=A   \/     ~                          ~           '. '.   .  .    .   .  .     :  ,';.     ,';.          ,;.'       B"
+	ECHO SET "layermap[1,2]=                    ~             \/               '. '.  .     .  .        . .:       ;.,'                ,;.'      "
+	ECHO SET "layermap[1,3]=      ~     \/            ~                 ~       :  '.  .  .    .   .    .-'      ,.'           ',.         ,;.'  "
+	ECHO SET "layermap[1,4]=  \/                              ~                 '. :'.  .  .  .       .-'   .,;.',;'       ',;.  ,';.       ,;.' "
+	ECHO SET "layermap[1,5]=         \/                                           :  '.  .   .     .--'  ,';.                     ,;.'    ,.';   "
+	ECHO SET "layermap[1,6]=  ~             ~         \/            ~      ~      '. :'.  .  . . .:', ; ;,           ,.'           ,;.'     ,;.' "
+	ECHO SET "layermap[1,7]=                    ~                                   '. '.   . .  :   ,;      ,;.'     ';        ,';.         ,;.'"
+	ECHO SET "layermap[1,8]=                                                         '. '. .   . :  ,';.        ',;.   ,;'                 ',;.,'"
+	ECHO SET "layermap[1,9]=    \/                                ~                   '. '.  . . :   ,;.                ';,.       ,;.';',,.',.  "
+	ECHO SET "layermap[1,10]=                           ~                               :  '. .  .:    ',;       W     A    T    E    R       ,';"
+	ECHO SET "layermap[1,11]=        ~                                ___               '. :'.  . : ,;.',;.' ,,;.' ,;.' ;.'                  ,';."
+	ECHO SET "layermap[1,12]=                 \/                     /   \      ~         '. '.   :   ';.';. ';.   ';.          ;.'    ;'.  ;.'  "
+	ECHO SET "layermap[1,13]=                                    \/ /_____\                '. '.  '-.           ;.'  ;.'      ;.'     ;.'  ';.   "
+	ECHO SET "layermap[1,14]=                                       :     :                 ': '..  :.;.;.'.'   ';.         ';.            ;.'  '"
+	ECHO SET "layermap[1,15]=                     ~                 :..   :    _             '.  '.. '--. ';.      ;.'             ;.'  ';. '; ;'"
+	ECHO SET "layermap[1,16]=     \/                                :::   :   ("^)              '. :'..  '-.       ';.  ;.';.'  ;.'       ;.'     "
+	ECHO SET "layermap[1,17]=                             \/     ~            -^!-                '': '..  '-.'. .   ';,.        ';.           .--"
+	ECHO SET "layermap[1,18]=              \/                                 /'\                  '.. '..  '-.  '    .,'      .,.'  .,' .---'  ."
+	ECHO SET "layermap[1,19]=                                            \/            ~             ':  :'.. '--   ,;'      .;'    .; -' .....' "
+	ECHO SET "layermap[1,20]=                                    \/                                    '.:.  '....,;.'    ',;.  ,;.'.....':      "
+	ECHO SET "layermap[1,21]=   ~           ~                      ~                                    : ': :. : :',.   ,';.    ;.: :..:'       "
+	ECHO SET "layermap[1,22]=                                                   \/                       :  ': : : ,'   ;,'     ;,' :'           "
+	ECHO SET "layermap[1,23]=                       \/       ~                                            :  ':. '  ',  ,'     ,;':.'            "
+	ECHO SET "layermap[1,24]=      \/     ~                              \/                    ~          :'. : .' ,'  ';.    ,;.: :             "
+	ECHO SET "layermap[1,25]=                                 \/                 \/                       : :'.': ',   ;'     ,;:'.'             "
+	ECHO SET "layermap[1,25]=                                 \/                 \/                       : :'.': ',   ;'     ,;:'.'             "
+	ECHO SET "layermap[1,26]=        \/      ~    \/                       ~                              : .': : ;'  ' ;.   '; :':              "
+	ECHO SET "layermap[1,27]=                                    \/                        ~               : '.' ;'   ,';.   ,';:'               "
+	ECHO SET "layermap[1,28]=        ~   \/                                                                : :'  ;',   ',.   ',;:                "
+	ECHO SET "layermap[1,29]=               /\/\/\        ~                \/    . - - - - .               : :  ,.;'   .;    ;',:                "
+	ECHO SET "layermap[1,30]=    \/        /\\/\//\             \/               :  .      :    ~        :'::   ,';.   ;',  ,;.':                "
+	ECHO SET "layermap[1,31]=  ~          /\ /\/\ /\    \/                    /\/\/\  ;,'  :            '.' : ,';.  ,';.   ,;.':                 "
+	ECHO SET "layermap[1,32]=        \/  ( //\/\/\\ )                  . - - /\\/\//\  \/  :           .'..:  ,';   ,.    ',.:  /\/\/\/\         "
+	ECHO SET "layermap[1,33]=  \/        (/ \/\/\/ \)                  :  ; /\ /\/\ /\  ; ':          '...:  ';.   ,;'   ,;.:  /\ /\/\ /\        "
+	ECHO SET "layermap[1,34]=             \\ /\/\ //         \/        : . ( //\/\/\\ )'   :         .:'.: ,';.  ,';.  ,';.'  (/ \/\/\/ \)       "
+	ECHO SET "layermap[1,35]= - - - - - - - \:  :/ - - - - - - - - - - '   (/ \/\/\/ \) ;. :         '.:   ,;.'   ,';.  ,';.   \\ /\/\ //        "
+	ECHO SET "layermap[1,36]=' ; \.    .   , :  :    ;./;   ; ; /,  ; .  '  \\ /\/\ /,;  ; :             ',;.'  ,;.'  ,;.'       \:  :/          "
+	ECHO SET "layermap[1,37]= . ; ;,\ ; /.   :  :   \;.  .\/;. ; /            \:  :/    \/ :          ,;.'   ;.,' ,;.'            :  :           "
+	ECHO SET "layermap[1,38]=  ; ' ;  ;\/    :  : ;.  ;./;    ;.  ;.\ \/ ;. '  :  :  ,  .  :        ,;.'  ,;.'   ,;.'             :  :           "
+	ECHO SET "layermap[1,39]= ; ;,   ;  ;/;. :  :    ;,  ;,\/;.        ;  .    :  : .,.    :     ,;.'. ,;.'  ,;.'                 :  :           "
+	ECHO SET "layermap[1,40]= ;. /./ / ;  \/.'  '\  /  ,  . /;   ,/ ;./    \   :  :   , '  :      ',;   ',;.  ',.                 '  '           "
+	ECHO SET "layermap[1,41]=' . /' .\;. /. ; ,'; \/ '  ' ;  ;./ .   ; ' ;    ':  :  \/.   ' - - .,;.,  ,;.'  ',;.                               "
+	ECHO SET "layermap[1,42]=./ . \/ . /  '  , , .,/  . ./, ,.\,./ ,.      ;   :  : .;   ;    '  :;.'  ',;.  ;. ,                                "
+	ECHO SET "layermap[1,43]=.  ,\/; ./., \/., . /. ., \,. ;/ , ,. \/,. '.  ;  '  '  ;   ' ,.'  ;:,    ;.',  ,;.'                                "
+	ECHO SET "layermap[1,44]=  \.,/  \. / ,'. \ ; , ' .\/  , .' \   .  ;  ;' .  ;  . ' ;\/ .,   .';- -'-;- - -;- - - - - - - - - - - - - - - - - "
+	ECHO SET "layermap[1,45]=;. \/ .,    , \/   ;. \/ ; ;.\/. , ; .   ;'/. ,  ';  .,. ,  '; .  ;,.'   ,';.  ,';.                                 "
+	ECHO SET "layermap[1,46]= ,. '  ; \/   ';  ,  / '   \;' ,  \/;'  ;  , \/  ;'  ,  ;  ,;  ' ,;.   ,';.     ,;.'                                "
+	ECHO SET "layermap[1,47]=  ;/  . '   ; '.    \ '  .' ;  '  . \/ '  .   ' ; '\/ ;  .'     ,;.'   ,;.'      ,;.'                               "
+	ECHO SET "layermap[1,48]=A. ;\/   \/',  ;'  .,  ;,\/     ;' ,   \/ ,;  '  ,    /  ;.   ,;.'     ,';.        ';.,                            B"
+) > .\buffer_layer_1.cmd
+
+SET POS_Y=1
+SET POS_X=1
+
+(
+	ECHO SET "layermap[2,1]=[1CwW[4CWw"
+	ECHO SET "layermap[2,2]=(O)\[2C/(O)"
+	ECHO SET "layermap[2,3]=[1C`. \/ .'"
+	ECHO SET "layermap[2,4]=[3C\  /"
+	ECHO SET "layermap[2,5]=[3C/  \"
+	ECHO SET "layermap[2,6]=[1C.' /\ `."
+	ECHO SET "layermap[2,7]=(_.'[2C`._)"
+) > .\buffer_layer_2.cmd
+
+(
+	ECHO SET "layermap[3,29]=[15C/\/\/\"
+	ECHO SET "layermap[3,30]=[14C/\\/\//\"
+	ECHO SET "layermap[3,31]=[13C/\ /\/\ /\"
+	ECHO SET "layermap[3,32]=[12C( //\/\/\\ )"
+	ECHO SET "layermap[3,33]=[12C(/ \/\/\/ \)"
+	ECHO SET "layermap[3,34]=[13C\\ /\/\ //"
+	ECHO SET "layermap[3,35]=[15C\:  :/"
+	ECHO SET "layermap[3,36]=[16C:  :"
+	ECHO SET "layermap[3,37]=[16C:  :"
+	ECHO SET "layermap[3,38]=[16C:  :"
+	ECHO SET "layermap[3,39]=[16C:  :"
+	ECHO SET "layermap[3,40]=[16C'  '"
+) > .\buffer_layer_3.cmd
+START /B "" CMD /C ".\coolrenderer.cmd" ^& EXIT
+
+
+:CHOICE
+%CHOICE%
+IF %CHOICE.INPUT%.==. (
+	DEL /Q ".\buffer_layer_1.cmd"
+	DEL /Q ".\buffer_layer_2.cmd"
+	DEL /Q ".\buffer_layer_3.cmd"
+	EXIT /B 0
+	EXIT /B 0
+	EXIT /B 0
+)
+IF /I %CHOICE.INPUT%==W (
+	SET /A POS_Y-=1
+	CALL :RENDER_PLAYER
+)
+IF /I %CHOICE.INPUT%==S (
+	SET /A POS_Y+=1
+	CALL :RENDER_PLAYER
+)
+IF /I %CHOICE.INPUT%==D (
+	SET /A POS_X+=2
+	CALL :RENDER_PLAYER
+)
+IF /I %CHOICE.INPUT%==A (
+	SET /A POS_X-=2
+	CALL :RENDER_PLAYER
+)
+GOTO CHOICE
+
+:RENDER_PLAYER
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET /A POS_Y_2=POS_Y+1
+SET /A POS_Y_3=POS_Y+2
+SET /A POS_Y_4=POS_Y+3
+SET /A POS_Y_5=POS_Y+4
+SET /A POS_Y_6=POS_Y+5
+SET /A POS_Y_7=POS_Y+6
+(
+	ECHO SET "layermap[2,%POS_Y%]=[%POS_X%C[1CwW[4CWw"
+	ECHO SET "layermap[2,%POS_Y_2%]=[%POS_X%C(O)\[2C/(O)"
+	ECHO SET "layermap[2,%POS_Y_3%]=[%POS_X%C[1C`. \/ .'"
+	ECHO SET "layermap[2,%POS_Y_4%]=[%POS_X%C[3C\  /"
+	ECHO SET "layermap[2,%POS_Y_5%]=[%POS_X%C[3C/  \"
+	ECHO SET "layermap[2,%POS_Y_6%]=[%POS_X%C[1C.' /\ `."
+	ECHO SET "layermap[2,%POS_Y_7%]=[%POS_X%C(_.'[2C`._)"
+) > .\buffer_layer_2.cmd
+ENDLOCAL
+EXIT /B 0
